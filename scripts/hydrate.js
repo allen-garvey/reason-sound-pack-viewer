361e5921936025e32abd833ecf556b8e23ff16e6
Allen Garvey <allengarvey@hotmail.com>
2021-03-17 13:41:20 -0400

    Create images directory before getting json

--- scripts/hydrate.js
+++ scripts/hydrate.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 const OUTPUT_DIR = path.join(__dirname, '..', 'public_html');
 const IMAGES_DIR = path.join(__dirname, '..', 'public_html', 'images');
 
-fetch('https://nautilus.reasonstudios.com/pack/')
+fs.promises.mkdir(IMAGES_DIR, {recursive: true})
+    .then(() => fetch('https://nautilus.reasonstudios.com/pack/'))
     .then(res => res.json())
     .then((packs) => {
         fs.promises.writeFile(path.join(OUTPUT_DIR, 'packs2.json'), JSON.stringify(packs))
