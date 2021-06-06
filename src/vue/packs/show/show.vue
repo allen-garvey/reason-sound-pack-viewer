0036a914c17da659d1ed08a18645a009f0076285
Allen Garvey <allengarvey@hotmail.com>
2021-06-06 09:29:15 -0400

    Refactor pack show page to up patch list component

--- src/vue/packs/show/show.vue
+++ src/vue/packs/show/show.vue
@@ -13,27 +13,18 @@
         />
         <p>{{ pack.author }}</p>
         <p>{{ pack.description }}</p>
-        <ul>
-            <li 
-                v-for="patch in pack.patches" 
-                :key="patch.id"
-                :class="$style.patch"
-            >
-                <span :class="$style.iconContainer" @click="patchClicked(patch)">
-                    <svg :class="$style.icon">
-                        <use xlink:href="#icon-play" v-if="!isPatchPlaying(patch)" />
-                        <use xlink:href="#icon-pause" v-if="isPatchPlaying(patch)" />
-                    </svg>
-                </span>
-                <span>{{ patch.name }}</span>
-            </li>
-        </ul>
+        <patch-list 
+            :patches="pack.patches"
+            :media-id="mediaId"
+            :parent-name="pack.title"
+            @audio-start="bubbleAudioStart"
+            @audio-stop="bubbleAudioStop"
+        />
     </main>
 </div>
 </template>
 
 <style lang="scss" module>
-$icon-controls-dimensions: 40px;
 $cover-image-dimensions: 300px;
 
 .main{
@@ -45,35 +36,13 @@ $cover-image-dimensions: 300px;
     width: $cover-image-dimensions;
 }
 
-.patch {
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    margin: 0 0 1em;
-}
-
-.iconContainer {
-    cursor: pointer;
-    display: inline-block;
-    margin-right: 2rem;
-    height: $icon-controls-dimensions;
-    width: $icon-controls-dimensions;
-
-    &:hover {
-        color: #666;
-    }
-}
-
-.icon {
-    max-height: 100%;
-    max-width: 100%;
-}
 </style>
 
 <script>
 import { AUDIO_PREVIEW_URL_PREFIX } from '../../../jukebox.js';
 import SiteTitle from '../../common/site-title.vue';
 import CoverImage from '../../common/cover-image.vue';
+import PatchList from '../../common/patch-list.vue';
 
 export default {
     props: {
@@ -88,10 +57,7 @@ export default {
     components: {
         SiteTitle,
         CoverImage,
-    },
-    data(){
-        return {
-        };
+        PatchList,
     },
     computed: {
         pack(){
@@ -102,12 +68,6 @@ export default {
         }
     },
     methods: {
-        patchId(patch){
-            return `${this.pack.id}-${patch.id}`;
-        },
-        isPatchPlaying(patch){
-            return this.patchId(patch) === this.mediaId;
-        },
         patchClicked(patch){
             if(this.isPatchPlaying(patch)){
                 this.$emit('audioStop');
@@ -120,17 +80,11 @@ export default {
                 });
             }
         },
-        packClicked(pack){
-            if(this.isPackPlaying){
-                this.$emit('audioStop');
-            }
-            else {
-                this.$emit('audioStart', {
-                    url: `${AUDIO_PREVIEW_URL_PREFIX}${this.pack.previewUrl}`,
-                    title: this.pack.title,
-                    id: this.pack.id,
-                });
-            }
+        bubbleAudioStart(event){
+            this.$emit('audioStart', event);
+        },
+        bubbleAudioStop(event){
+            this.$emit('audioStop');
         },
     },
 };
