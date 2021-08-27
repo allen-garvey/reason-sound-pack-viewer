ced04990ff16a051526bc6d2e3cf42e480a13128
Allen Garvey <allengarvey@hotmail.com>
2021-08-27 09:10:47 -0400

    Add more author sites

--- src/author-sites.js
+++ src/author-sites.js
@@ -1,6 +1,13 @@
 export default {
     'Bryn Bliska': 'https://www.brynbliska.com/',
+    'DNA Labs': 'https://www.dnalabssoftware.com/',
+    'Josh Mobley Music': 'https://www.joshmobley.com/',
     'Kickback Couture': 'https://kickbackcouture.com/',
+    'Lab One Recordings': 'https://lab-one-recordings.co.uk/',
+    'Melodiefabriek': 'https://melodiefabriek.com/',
+    'ModeAudio': 'https://modeaudio.com/',
+    'New Loops': 'https://newloops.com/',
+    'Paul Ortiz': 'https://paulortiz.bandcamp.com/',
     'Seán Murray': 'https://www.seanmurrayguitar.com/',
     'SKP Sound Design': 'https://www.skpsounds.com/',
     'Venus Theory': 'https://venustheory.com/',
