46492a046fb2d01cac5e17f0ae82d18c869488a3
Allen Garvey <allengarvey@hotmail.com>
2023-12-19 14:10:10 -0500

    Update to use new api

--- scripts/hydrate.mjs
+++ scripts/hydrate.mjs
@@ -7,41 +7,71 @@ import { normalizeApiJson } from './normalize-api-json.mjs';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
+const API_PATH = 'https://nautilus.reasonstudios.com/pack';
 const OUTPUT_DIR = path.join(__dirname, '..', 'public_html');
 const IMAGES_DIR = path.join(__dirname, '..', 'public_html', 'images');
 const IMAGE_DIMENSIONS = 300;
 
 const packImageName = (pack) => `pack-${pack.id}.webp`;
 
-const createImageCacheSet = () => 
-    fs.promises.readdir(IMAGES_DIR)
-    .then((files) => Promise.resolve(new Set(files)));
+const createImageCacheSet = () =>
+    fs.promises
+        .readdir(IMAGES_DIR)
+        .then((files) => Promise.resolve(new Set(files)));
 
-const writePackImages = (packs, imageSet) => packs.map((pack) => {
-    const imageName = packImageName(pack);
-    if(imageSet.has(imageName)){
-        return Promise.resolve();
-    }
-    return fetch(pack.coverPhoto)
-        .then(res => res.arrayBuffer())
-        .then((data) => sharp(Buffer.from(data))
-            .resize({width: IMAGE_DIMENSIONS, height: IMAGE_DIMENSIONS, quality: 85})
-            .toFile(path.join(IMAGES_DIR, imageName)))
-        .then(() => 1);
-});
+const writePackImages = (packs, imageSet) =>
+    packs.map((pack) => {
+        const imageName = packImageName(pack);
+        if (imageSet.has(imageName)) {
+            return Promise.resolve();
+        }
+        return fetch(pack.coverPhoto)
+            .then((res) => res.arrayBuffer())
+            .then((data) =>
+                sharp(Buffer.from(data))
+                    .resize({
+                        width: IMAGE_DIMENSIONS,
+                        height: IMAGE_DIMENSIONS,
+                        quality: 85,
+                    })
+                    .toFile(path.join(IMAGES_DIR, imageName))
+            )
+            .then(() => 1);
+    });
 
 Promise.all([
-    fetch('https://nautilus.reasonstudios.com/pack/'),
-    fs.promises.mkdir(IMAGES_DIR, {recursive: true})
+    fetch(API_PATH),
+    fs.promises.mkdir(IMAGES_DIR, { recursive: true }),
 ])
     .then(([res]) => Promise.all([res.json(), createImageCacheSet()]))
     .then(([packs, imageSet]) => {
+        const packsPromises = Promise.all(
+            packs.map((pack) =>
+                fetch(`${API_PATH}/${pack.id}`).then((res) => res.json())
+            )
+        );
+        return Promise.all([packsPromises, Promise.resolve(imageSet)]);
+    })
+    .then(([packs, imageSet]) => {
         const promises = writePackImages(packs, imageSet);
-        promises.push(fs.promises.writeFile(path.join(OUTPUT_DIR, 'packs.json'), JSON.stringify(normalizeApiJson(packs))));
-        
+        promises.push(
+            fs.promises.writeFile(
+                path.join(OUTPUT_DIR, 'packs.json'),
+                JSON.stringify(normalizeApiJson(packs))
+            )
+        );
+
         return Promise.all(promises);
     })
     .then((results) => {
-        const amountUpdated = results.reduce((total, currentValue) => currentValue === 1 ? total + currentValue : total, 0);
-        console.log(`${amountUpdated} pack cover${amountUpdated === 1 ? '' : 's'} downloaded`);
+        const amountUpdated = results.reduce(
+            (total, currentValue) =>
+                currentValue === 1 ? total + currentValue : total,
+            0
+        );
+        console.log(
+            `${amountUpdated} pack cover${
+                amountUpdated === 1 ? '' : 's'
+            } downloaded`
+        );
     });
